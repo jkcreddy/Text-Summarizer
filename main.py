0e885878bafd7fb23932f52df44132b2a41bf323
Krishna Chaitanya J <jkc.reddy@gmail.com>
2024-12-29 15:38:38 -0600

    Model training added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from textSummarizer.logging import logger
+import os
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -32,6 +34,18 @@ STAGE_NAME = "Data Transformatoin stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
     data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nX==================X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Training stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+    os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
+    data_transformation = ModelTrainerTrainingPipeline()
     data_transformation.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nX==================X")
 except Exception as e:
