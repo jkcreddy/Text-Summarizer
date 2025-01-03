d7c82a36390b0562427ff88cc635551d776a9122
Krishna Chaitanya J <jkc.reddy@gmail.com>
2025-01-02 22:56:31 -0600

    model evaluation completed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainin
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textSummarizer.logging import logger
 import os
 
@@ -47,6 +48,17 @@ try:
     os.environ["PYTORCH_MPS_HIGH_WATERMARK_RATIO"] = "0.0"
     data_transformation = ModelTrainerTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nX==================X")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+    model_evaluation = ModelEvaluationTrainingPipeline()
+    model_evaluation.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nX==================X")
 except Exception as e:
     logger.exception(e)
